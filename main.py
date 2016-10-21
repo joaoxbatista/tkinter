7854fe4199d42b5ae396367b6203be67dcdfc00b
João Batista Gomes Silva <jhonxbatista@gmail.com>
2016-10-21 14:22:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #-*- coding:latin1 -*-
+#Authors: Joao Batista Gomes Silva, Erick Oliveira de Souza
+#Github: jhonxbatista
+#E-mails: jhonxbatista@gmail.com, erickdynsouza@hotmail.com
 
 from Tkinter import *
 from Janela import *
